75afa2bf6943283b45e40aaed735219b968f427e
nicrodriguezval <nicrodriguezval@gmail.com>
2024-01-19 22:53:39 -0500

    Postgres database configured using Docker

--- database/up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    slug VARCHAR NOT NULL,
-    body TEXT NOT NULL
-);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let db_url = env::var("DB_URL")
+        .expect("DATABASE_URL must be set");
+
+    println!("DB_URL: {:?}", db_url);
 }
