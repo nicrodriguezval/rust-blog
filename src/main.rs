0bac799e59fbd114443b53075f5cc8846b693f0a
nicrodriguezval <nicrodriguezval@gmail.com>
2024-01-23 18:49:31 -0500

    Post model defined

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+#[macro_use]
+extern crate diesel;
+
+mod schema;
+mod models;
+
 use std::env;
 use dotenv::dotenv;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+use schema::posts::dsl::posts;
+use models::post::Post;
 
 fn main() {
     dotenv().ok();
@@ -7,5 +18,15 @@ fn main() {
     let db_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
 
-    println!("DB_URL: {:?}", db_url);
+    let mut conn = PgConnection::establish(&db_url)
+        .expect(&format!("Error connecting to {}", db_url));
+
+    // SELECT * FROM posts;
+    let posts_result = posts
+        .load::<Post>(&mut conn)
+        .expect("Error loading posts");
+
+    for post in &posts_result {
+        println!("{}: {}", post.id, post.title);
+    }
 }
