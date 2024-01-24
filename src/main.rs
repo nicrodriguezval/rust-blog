0943dd7358898bd90b21f7daf54c3022b1f2b2c7
nicrodriguezval <nicrodriguezval@gmail.com>
2024-01-23 20:08:51 -0500

    Update query added

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use dotenv::dotenv;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
-use models::post::{Post, SimplePost};
+use models::post::{Post};
 use schema::posts;
 
 fn main() {
@@ -29,31 +29,49 @@ fn main() {
     //     body: "This is my first post",
     // };
 
-    // let _posts = diesel::insert_into(posts::table)
+    // diesel::insert_into(posts::table)
     //     .values(&new_post)
     //     .get_result::<Post>(&mut conn)
     //     .expect("Error saving new post");
 
     // SELECT * FROM posts WHERE id = 5 AND title = 'Hello, world!';
+    // let posts_result = posts::table
+    //     .find(5)
+    //     .filter(posts::title.eq("Hello, world!"))
+    //     .load::<Post>(&mut conn)
+    //     .expect("Error loading posts");
+
+    // for post in posts_result.into_iter() {
+    //     println!("{:?}", post)
+    // }
+
+    // SELECT id, title FROM posts ORDER BY id DESC LIMIT 5;
+    // let posts_result = posts::table
+    //     .select((posts::id, posts::title))
+    //     .order(posts::id.desc())
+    //     .limit(5)
+    //     .load::<SimplePost>(&mut conn)
+    //     .expect("Error loading posts");
+
+    // for post in &posts_result {
+    //     println!("{:?}", post)
+    // }
+
+    // UPDATE posts SET title = $1, slug = $2 WHERE id = $3 RETURNING id, title, slug, body;
+    diesel::update(posts::table.find(5))
+        .set((
+            posts::title.eq("Hello, world. Updated!"),
+            posts::slug.eq("hello-world-updated"),
+        ))
+        .get_result::<Post>(&mut conn)
+        .expect("Error saving new post");
+
+    // SELECT * FROM posts;
     let posts_result = posts::table
-        .find(5)
-        .filter(posts::title.eq("Hello, world!"))
         .load::<Post>(&mut conn)
         .expect("Error loading posts");
 
     for post in posts_result.into_iter() {
-        println!("{:?}", post)
-    }
-
-    // SELECT id, title FROM posts ORDER BY id DESC LIMIT 5;
-    let posts_result = posts::table
-        .select((posts::id, posts::title))
-        .order(posts::id.desc())
-        .limit(5)
-        .load::<SimplePost>(&mut conn)
-        .expect("Error loading posts");
-
-    for post in &posts_result {
         println!("{:?}", post)
     }
 }
